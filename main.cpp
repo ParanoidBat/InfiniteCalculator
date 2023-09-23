cc5f62230df5e6da4e400f4deaac71260ea05f75
omar azaz <cool_omer@hotmail.co.uk>
2023-09-23 23:58:20 +0500

    extend brackets

--- main.cpp
+++ main.cpp
@@ -10,6 +10,25 @@ bool is_num(char x){
     return (int)x >= 48 && (int)x <= 57;
 }
 
+bool is_opening_bracket(char in){
+    return in == '(' || in == '{' || in == '[';
+}
+
+bool is_closing_bracket(char in){
+    return in == ')' || in == '}' || in == ']';
+}
+
+char get_opening_bracket(char in){
+    switch(in){
+    case ')':
+        return '(';
+    case '}':
+        return '{';
+    case ']':
+        return '[';
+    }
+}
+
 int perform_calculation(char op, int operand1, int operand2){
     switch(op){
     case '+':
@@ -35,9 +54,11 @@ void calculate(){
 }
 
 int main(){
-    string input = "(1+2)*3+5";
+    string input = "[1+2*(3-2)+1]-2";
     unordered_map<char, short> op_prec;
 
+    op_prec['['] = 7;
+    op_prec['{'] = 6;
     op_prec['('] = 5;
     op_prec['/'] = 4;
     op_prec['*'] = 3;
@@ -50,14 +71,15 @@ int main(){
         }
         else if(!operators.empty()){
             char tos = operators.top();
-            if (in == ')'){
-                while (!operators.empty() && operators.top() != '('){
+            if (is_closing_bracket(in)){
+                char opening_bracket = get_opening_bracket(in);
+                while (!operators.empty() && operators.top() != opening_bracket){
                     calculate();
                 }
-                operators.pop(); // pop the ( as well
+                operators.pop(); // pop the opening bracket as well
             }
-            else if (op_prec[in] < op_prec[tos] && tos != '('){
-                while (!operators.empty() && op_prec[in] <= op_prec[operators.top()]){
+            else if (op_prec[in] < op_prec[tos] && !is_opening_bracket(tos)){
+                while (!operators.empty() && !is_opening_bracket(operators.top()) && op_prec[in] <= op_prec[operators.top()]){
                     calculate();
                 }
                 operators.push(in);
@@ -72,11 +94,12 @@ int main(){
     }
 
     while (!operators.empty()){
-        if (operators.top() == ')'){
-            while (!operators.empty() && operators.top() != '('){
+        if (is_closing_bracket(operators.top())){
+            char opening_bracket = get_opening_bracket(operators.top());
+            while (!operators.empty() && operators.top() != opening_bracket){
                 calculate();
             }
-            operators.pop(); // pop the ( as well
+            operators.pop(); // pop the opening bracket as well
         }
         else{
             calculate();
