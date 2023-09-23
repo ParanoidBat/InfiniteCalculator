ab74df3a3bbeb635ca38586fab5e9ade444f033a
omar azaz <cool_omer@hotmail.co.uk>
2023-09-23 08:10:21 +0500

    calculate expression

--- main.cpp
+++ main.cpp
@@ -7,11 +7,26 @@ bool is_num(char x){
     return (int)x >= 48 && (int)x <= 57;
 }
 
+int calculate(char op, int operand1, int operand2){
+    switch(op){
+    case '+':
+        return operand1 + operand2;
+    case '-':
+        return operand1 - operand2;
+    case '*':
+        return operand1 * operand2;
+    case '/':
+        return operand1 / operand2;
+    }
+}
+
 int main(){
     string input = "(1+2)*3+5";
     string expression = "";
     stack <char> operators;
+    stack <int> numbers;
     unordered_map<char, short> op_prec;
+    int res, operand1, operand2;
 
     op_prec['('] = 5;
     op_prec['/'] = 4;
@@ -21,20 +36,32 @@ int main(){
 
     for (char in : input){
         if(is_num(in)){
-            expression += in;
+            numbers.push((int)in - 48); // put in the actual number, not the ASCII
         }
         else if(!operators.empty()){
             char tos = operators.top();
             if (in == ')'){
                 while (!operators.empty() && operators.top() != '('){
-                    expression += operators.top();
+                    operand2 = numbers.top();
+                    numbers.pop();
+                    operand1 = numbers.top();
+                    numbers.pop();
+
+                    res = calculate(operators.top(), operand1, operand2);
+                    numbers.push(res);
                     operators.pop();
                 }
                 operators.pop(); // pop the ( as well
             }
             else if (op_prec[in] < op_prec[tos] && tos != '('){
                 while (!operators.empty() && op_prec[in] <= op_prec[operators.top()]){
-                    expression += operators.top();
+                    operand2 = numbers.top();
+                    numbers.pop();
+                    operand1 = numbers.top();
+                    numbers.pop();
+
+                    res = calculate(operators.top(), operand1, operand2);
+                    numbers.push(res);
                     operators.pop();
                 }
                 operators.push(in);
@@ -49,18 +76,16 @@ int main(){
     }
 
     while (!operators.empty()){
-        expression += operators.top();
+        operand2 = numbers.top();
+        numbers.pop();
+        operand1 = numbers.top();
+        numbers.pop();
+
+        res = calculate(operators.top(), operand1, operand2);
+        numbers.push(res);
         operators.pop();
     }
-    cout<< expression;
-
-    string final = "";
-    for(int i = expression.length - 1; i >= 2; i--){ // the first two elements will be digits
-        // if this is operator, check if next two are operands, without going out of bounds
-        if(!is_num(expression[i]) && i-2 >= 0 && is_num(expression[i-1]) && is_num(expression[i-2]) ){
-
-        }
-    }
+    cout<<"res: "<<numbers.top();
 
     return 0;
 }
