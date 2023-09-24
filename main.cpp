7352e6b0ac904b93ace074f2881a5c37c4f1e19a
omar azaz <cool_omer@hotmail.co.uk>
2023-09-24 06:16:18 +0500

    string input operands

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <stack>
+#include <string>
+#include <stdexcept>
 #include <unordered_map>
+
 using namespace std;
 
 stack <char> operators;
-stack <int> numbers;
+stack <string> numbers;
 
 bool is_num(char x){
     return (int)x >= 48 && (int)x <= 57;
@@ -43,19 +46,21 @@ int perform_calculation(char op, int operand1, int operand2){
 }
 
 void calculate(){
-    char operand2 = numbers.top();
+    string operand2 = numbers.top();
     numbers.pop();
-    char operand1 = numbers.top();
+    string operand1 = numbers.top();
     numbers.pop();
 
-    int res = perform_calculation(operators.top(), operand1, operand2);
-    numbers.push(res);
-    operators.pop();
+//    int res = perform_calculation(operators.top(), operand1, operand2);
+//    numbers.push(res);
+//    operators.pop();
 }
 
 int main(){
-    string input = "[1+2*(3-2)+1]-2";
+    string input = "25+50";
+    char in;
     unordered_map<char, short> op_prec;
+    bool isNum = false;
 
     op_prec['['] = 7;
     op_prec['{'] = 6;
@@ -65,9 +70,29 @@ int main(){
     op_prec['+'] = 2;
     op_prec['-'] = 1;
 
-    for (char in : input){
+    for (int i = 0; i < input.length(); i++){
+        in = input.at(i);
+
         if(is_num(in)){
-            numbers.push((int)in - 48); // put in the actual number, not the ASCII
+            numbers.push("");
+            char n;
+            while(1){
+                try{
+                    n = input.at(i);
+                }
+                catch(const out_of_range& err){
+                    break;
+                }
+
+                if(is_num(n)){
+                    numbers.top().push_back(n);
+                    i++;
+                }
+                else{
+                    i--; // restore i to previous value, lest the operator is skipped
+                    break;
+                }
+            }
         }
         else if(!operators.empty()){
             char tos = operators.top();
@@ -105,7 +130,7 @@ int main(){
             calculate();
         }
     }
-    cout<<"res: "<<numbers.top();
+//    cout<<"res: "<<numbers.top();
 
     return 0;
 }
